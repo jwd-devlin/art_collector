2c8ba279aa3d9a69992f4d0d08026178c8c218e1
jwd <jwd.devlin@gmail.com>
2021-03-06 23:34:24 +0100

    Update tests

--- src/tests/data_pipeline/data_transform/test_dimension_extractor.py
+++ src/tests/data_pipeline/data_transform/test_dimension_extractor.py
@@ -9,39 +9,53 @@ class TestExecute(unittest.TestCase):
 
     def test_two_sets_of_diameters_data(self):
         input_0 = "a: H. 1 3/8 in. (3.5 cm); Diam. 1 5/16 in. (3.3 cm)\r\nb: Diam. 2 in. (5.1 cm)"
-        expected = {"Length": 5.1, "Width": 5.1, "Height": 3.5}
+        expected = {"art_length": 5.1, "art_width": 5.1, "art_height": 3.5}
         output = self.extractor.extractation(input_0)
         self.assertEqual(expected, output)
 
     def test_two_measurement_direction_values_no_direction_flags(self):
         input_0 = '12 1/2 x 7 7/8 in. (31.8 x 20 cm)'
-        expected = {"Length": 31.8, "Width": 20}
+        expected = {"art_length": 31.8, "art_width": 20}
         output = self.extractor.extractation(input_0)
         self.assertEqual(expected, output)
 
     def test_three_measurement_direction_values_no_direction_flags(self):
         input_0 = "34 3/4 x 32 3/8 x 4 in. (88.3 x 82.2 x 10.2 cm)"
-        expected = {"Length": 88.3, "Width": 82.2, "Height": 10.2}
+        expected = {"art_length": 88.3, "art_width": 82.2, "art_height": 10.2}
         output = self.extractor.extractation(input_0)
         self.assertEqual(expected, output)
 
     def test_one_value_and_measurement_direction_flag(self):
         input_1 = 'H. 12 3/4 in. (32.4 cm)'
-        expected_1 = {"Height": 32.4}
+        expected_1 = {"art_height": 32.4}
         output_1 = self.extractor.extractation(input_1)
         self.assertEqual(expected_1, output_1)
 
         input_2 = 'Diam. 5 3/4 in. (14.6 cm)'
-        expected_2 = {"Length": 14.6, "Width": 14.6}
+        expected_2 = {"art_length": 14.6, "art_width": 14.6}
         output_2 = self.extractor.extractation(input_2)
         self.assertEqual(expected_2, output_2)
 
     def test_overall_with_multi_sets_of_dimension_data(self):
         input_0 = 'Overall: 3 x 6 1/2 in. (7.6 x 16.5 cm); ' \
                   '4 oz. 10 dwt. (140.6 g)\r\nLip: Diam. 4 1/16 in. (10.3 cm)\r\nFoot: Diam. 2 3/4 in. (7 cm)'
-        expected = {"Length": 7.6, "Width": 16.5}
+        expected = {"art_length": 7.6, "art_width": 16.5}
         output = self.extractor.extractation(input_0)
         self.assertEqual(expected, output)
 
+    def test_different_string_for_direction_split(self):
+        input_0 = '24 1/2 in. × 13 in. × 20 3/8 in. (62.2 × 33 × 51.8 cm)'
+        expected = {"art_length": 62.2, "art_width": 33.0, "art_height": 51.8}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_different_string_for_direction_split_1(self):
+        input_0 = '6 x 4 x 1 in. (15.2 x 10.2 x 2.5 cm)'
+        expected = {"art_length": 15.2, "art_width": 10.2, "art_height": 2.5}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    # H. 4 1/4 in. (10.8); Diam. 14 1/2 in. (36.8 cm)
+    # Overall: 7 3/4 in., 220 Grams (19.7 cm, 7.073 Troy Ounces)
 if __name__ == '__main__':
     unittest.main()
--- src/tests/data_pipeline/data_transform/test_met_art_transform.py
+++ src/tests/data_pipeline/data_transform/test_met_art_transform.py
@@ -12,9 +12,9 @@ class TestExecute(unittest.TestCase):
     def test_no_dimension_data(self):
         df_test = pd.DataFrame({'Object ID': {0: 1},
                                 'Dimensions': {0: 'Dimensions unavailable'}})
-        df_output_true = pd.DataFrame({'Object ID': {0: 1},
-                                       'Dimensions': {0: 'Dimensions unavailable'},
-                                       'Length': {0: 0.0}, 'Width': {0: 0.0}, 'Height': {0: 0.0}})
+        df_output_true = pd.DataFrame({'object_id': {0: 1},
+                                       'dimensions': {0: 'Dimensions unavailable'},
+                                       'art_length': {0: 0.0}, 'art_width': {0: 0.0}, 'art_height': {0: 0.0}})
         df_output = self.transformer.execute(df_test)
         print(df_output)
         assert_frame_equal(df_output_true, df_output)
@@ -22,9 +22,9 @@ class TestExecute(unittest.TestCase):
     def test_dimensions_data(self):
         df_test = pd.DataFrame({'Object ID': {0: 1},
                                 'Dimensions': {0: 'H. 6 3/4 in. (17.1 cm); Diam. 3 3/8 in. (8.6 cm)'}})
-        df_output_true = pd.DataFrame({'Object ID': {0: 1},
-                                       'Dimensions': {0: 'H. 6 3/4 in. (17.1 cm); Diam. 3 3/8 in. (8.6 cm)'},
-                                       'Length': {0: 8.6}, 'Width': {0: 8.6}, 'Height': {0: 17.1}})
+        df_output_true = pd.DataFrame({'object_id': {0: 1},
+                                       'dimensions': {0: 'H. 6 3/4 in. (17.1 cm); Diam. 3 3/8 in. (8.6 cm)'},
+                                       'art_length': {0: 8.6}, 'art_width': {0: 8.6}, 'art_height': {0: 17.1}})
         df_output = self.transformer.execute(df_test)
         assert_frame_equal(df_output_true, df_output)
 
