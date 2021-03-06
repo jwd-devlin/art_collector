e2a29cc03c632bf7955e0da531403ec645f6e999
jwd <jwd.devlin@gmail.com>
2021-03-06 23:33:46 +0100

    Add updates for upload

--- src/main/data_pipeline/data_transform/met_art_transform.py
+++ src/main/data_pipeline/data_transform/met_art_transform.py
@@ -5,17 +5,19 @@ import logging
 
 class MetArtTransform:
     # Input Conditions
-    DIMENSIONS_RAW_DATA_FIELD = "Dimensions"
-    ART_ID = "Object ID"
+    DIMENSIONS_RAW_DATA_FIELD = "dimensions"
+    ART_ID = "object_id"
 
     # Output Details
-    DIMENSION_OUTPUT_FIELDS = ["Length", "Width", "Height"]
+    DIMENSION_OUTPUT_FIELDS = ["art_length", "art_width", "art_height"]
 
     # # Cleaning Details
     # No Dimensions
     NONE_DIMENSIONS_FLAGS = ["Dimensions unavailable"]
 
     DEFAULT_EMPTY_VALUE = 0
+
+    RENAME_COLUMNS = {"Object ID": ART_ID, "Dimensions": DIMENSIONS_RAW_DATA_FIELD}
 
     def __init__(self):
         self.dimension_extractor = DimensionExtractor()
@@ -32,6 +34,8 @@ class MetArtTransform:
         return [cleaned_dimensions.get(field, self.DEFAULT_EMPTY_VALUE) for field in self.DIMENSION_OUTPUT_FIELDS]
 
     def execute(self, art_data: pd.DataFrame) -> pd.DataFrame:
+        # Remove empty values
+        art_data = art_data.rename(columns=self.RENAME_COLUMNS).dropna(how="any", axis=0)
 
         self.__assign_empty_dimensions_placeholders(art_data)
 
@@ -39,5 +43,12 @@ class MetArtTransform:
             if self.__check_known_no_dimensions(row[self.DIMENSIONS_RAW_DATA_FIELD]):
                 logging.info("%s has been flagged as no dimensions found.", row[self.ART_ID])
                 continue
-            art_data.loc[index, self.DIMENSION_OUTPUT_FIELDS] = self.__extract_data(row[self.DIMENSIONS_RAW_DATA_FIELD])
+
+            try:
+                extracted_dimensions = self.__extract_data(row[self.DIMENSIONS_RAW_DATA_FIELD])
+            except:
+                logging.warning("Skipping % has dimension string %s", row[self.ART_ID],
+                                row[self.DIMENSIONS_RAW_DATA_FIELD])
+                extracted_dimensions = [0, 0, 0]
+            art_data.loc[index, self.DIMENSION_OUTPUT_FIELDS] = extracted_dimensions
         return art_data[[self.ART_ID, self.DIMENSIONS_RAW_DATA_FIELD, *self.DIMENSION_OUTPUT_FIELDS]]
