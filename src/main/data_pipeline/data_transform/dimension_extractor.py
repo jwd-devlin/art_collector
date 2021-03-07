1c6cf487dae9228524f140d15d4c40ee16a5331c
jwd <jwd.devlin@gmail.com>
2021-03-07 19:33:42 +0100

    Data extraction

--- src/main/data_pipeline/data_transform/dimension_extractor.py
+++ src/main/data_pipeline/data_transform/dimension_extractor.py
@@ -22,105 +22,134 @@ class DimensionExtractor:
 
     DEFAULT_MEASUREMENT_DIRECTIONS = ["art_length", "art_width", "art_height"]
     # Data with multi fields commonly split by:
-    DIMENSION_SPLITS = [";", "\r\n", ":"]
-    MULTI_DIMENSIONS_SPLIT = ["x", "×"]
+    DIMENSION_SPLITS = [";", "\r\n", "mount:", "frame:", "\n"]
+    MULTI_DIMENSIONS_SPLIT = ["x", "×", "-", ","]
 
-    POS_ID_HEIGHT_FLAG = "H. "
-    POS_ID_LENGTH_FLAG = "L. "
-    POS_ID_DIAMETER_FLAG = "Diam"
-    MEASUREMENT_DIRECTIONS_BY_FLAGS = {"H. ": ["art_height"], "L. ": ["art_length"],
-                                       "Diam": ["art_length", "art_width"]}
+    # Identify direction of measurement
+    MEASUREMENT_DIRECTIONS_BY_FLAGS = {"h.": ["art_height"], "l.": ["art_length"], "h:": ["art_height"],
+                                       "diam": ["art_length", "art_width"], "height:": ["art_height"],
+                                       "th.": ["art_width"], "d.": ["art_length", "art_width"], "w.": ["art_width"],
+                                       " h ": ["art_height"], "d ": ["art_length", "art_width"],
+                                       "width:": ["art_width"], "length:": ["art_width"], "w:": ["art_width"]}
 
-    OVERALL_FLAG = "Overall:"
+    RANGED_VALUES = ["to", "-"]
 
+    OVERALL_FLAG = "overall:"
+    IMPERIAL_UNITS = ["in.", "inches", "inch"]
+    IMPERIAL_UNIT = "in"
+
+    # CLEAN_METRIC_NUMBERS = ["th.", "l.", "inside diameter", "diameter", "h.", "Diam.", "cm.", "image:"]
     METRIC_UNIT = "cm"
-    INBRACKETS_MATCH = """\((.*cm)"""
+
+    # Regex Match Patterns
+    CM_BRACKETS_MATCH = """\((.*cm)"""
+    ONLY_TEXT_BRACKETS = """\([a-z]+\)"""
+    INBRACKETS_MATCH = """\(.*?\)"""
+    WIEGHT_MATCH = """(?<=cm)(.*)(?=g*)"""
+    NUMBER_EXTRACT = """\d+\.\d+|\d+"""
+
+    def __init__(self):
+        self.search_imperial = re.compile('|'.join(self.IMPERIAL_UNITS))
+
+    def __get_imperial_text(self, text_brackets: list) -> list:
+        imperial_brackets = []
+        for text in text_brackets:
+            if re.search(self.search_imperial, text):
+                imperial_brackets.append(text)
+        return imperial_brackets
+
+    def __get_metric_text(self, text_brackets: list) -> list:
+        metric_brackets = []
+        for text in text_brackets:
+            if self.METRIC_UNIT in text:
+                metric_brackets.append(re.findall(self.CM_BRACKETS_MATCH, text)[0])
+        return "x".join(metric_brackets)
+
+    def __find_cm_data_in_or_out_brackets(self, text_brackets: list, dimensions: str) -> str:
+
+        # No brackets
+        if not text_brackets:
+            # Remove any additional associated weight data.
+            weight_info = re.findall(self.WIEGHT_MATCH, dimensions)
+            if weight_info and "g" in dimensions:
+                return re.sub(weight_info[0], '', dimensions)
+            return dimensions
+
+        # Metric data in brackets
+        metric_string = self.__get_metric_text(text_brackets)
+        if metric_string:
+            return metric_string
+
+        # if imperial units in brackets.
+        imperial_brackets = self.__get_imperial_text(text_brackets)
+        if imperial_brackets:
+            return re.sub("|".join(imperial_brackets), '', dimensions).replace("()", "").strip()
 
     def __extract_cm_data(self, dimensions: str) -> list:
 
         """
-        Example:
-        '12 1/2 x 7 7/8 in. (31.8 x 20 cm)' -> '31.8 x 20' -> [31.8, 20]
+        Example in brackets:
+
+         - '12 1/2 x 7 7/8 in. (31.8 x 20 cm)' -> '31.8 x 20' -> [31.8, 20]
+
+        Example imperial in brackets:
+
+        -
+
+
         """
 
-        set_extracted = re.findall(self.INBRACKETS_MATCH, dimensions)
+        in_brackets_text = re.findall(self.INBRACKETS_MATCH, dimensions)
+        cm_extracted = self.__find_cm_data_in_or_out_brackets(in_brackets_text, dimensions)
 
-        if len(set_extracted) > 1:
-            logging.warning(" more DIMENSION_SPLITS found for : %s, Only taking the first.", dimensions)
+        # clean_extracted = re.sub("|".join([self.METRIC_UNIT, *self.CLEAN_METRIC_NUMBERS]), '', cm_extracted).strip(".").strip()
 
-        clean_extracted = re.sub(self.METRIC_UNIT, '', set_extracted[0]).strip()
-        try:
-            re.split("|".join(self.MULTI_DIMENSIONS_SPLIT), clean_extracted)
-        except:
-            print(dimensions)
+        cm_extracted = re.split("|".join(self.MULTI_DIMENSIONS_SPLIT), cm_extracted)
 
-        return re.split("|".join(self.MULTI_DIMENSIONS_SPLIT), clean_extracted)
+        clean_extracted = [re.findall(self.NUMBER_EXTRACT, item) for item in cm_extracted]
+        return [item[0] for item in clean_extracted if item]
 
-    def __get_direction_id_flag(self, text: str) -> str:
+    def __get_direction_id_flags(self, text: str) -> list:
+        """
+
+        Get direction flags and order.
+
+        Should only be
+
+        """
+
+        flags = []
+        order = []
         for key in self.MEASUREMENT_DIRECTIONS_BY_FLAGS.keys():
             if key in text:
-                return key
-        return ""
+                flags.append(key)
+                order.append(text.find(key))
+
+        if not flags:
+            return flags
+
+        return [x for _, x in sorted(zip(order, flags))]
 
     def __all_measurement_directions(self, dimensions_split: list) -> bool:
         return len(dimensions_split) == 3
 
-    def __has_diameter_flag(self, positions_flags:str) -> bool:
-        return self.POS_ID_DIAMETER_FLAG in positions_flags
+    def __map_cm_data_to_direction_flags(self, measurement_direction_flags: list, cm_data_split: list,
+                                         out_dimensions: dict):
 
-    def __get_measurement_directions(self, flag: str, number_dimension_values: int) -> list:
-        """
+        for index, value in enumerate(cm_data_split):
+            if measurement_direction_flags:
+                flag = measurement_direction_flags[index]
+                directions = self.MEASUREMENT_DIRECTIONS_BY_FLAGS[flag]
+            else:
+                # default to direction based on position
+                directions = [self.DEFAULT_MEASUREMENT_DIRECTIONS[index]]
 
-        Returns the possible measurement directions: ["art_length", "art_width", "art_height"]
-
-        Example 1.:
-            flag = ""
-            number_dimension_values = 3
-              -> ["art_length", "art_width", "art_height"]
-
-        Example 2:
-            flag = ""
-            number_dimension_values = 2
-              -> ["art_length", "art_width"]
-
-        Example 2:
-            flag = "H"
-            number_dimension_values = 1
-              -> ["art_height"]
-        """
-        measurement_directions = self.MEASUREMENT_DIRECTIONS_BY_FLAGS.get(flag, self.DEFAULT_MEASUREMENT_DIRECTIONS)
-        return measurement_directions[0:number_dimension_values]
-
-    def __update_out_dimensions_by_order(self, split_dimensions: list, out_dimensions: dict,
-                                         measurement_direction_flag: str = "") -> None:
-        """
-        Example 1:
-        Empty out dimensions, simple fill in:
-
-        out_dimensions = {}
-        split_dimensions = [97.2, 56.5,52.7]
-        measurement_directions = "Diam"
-
-        -> {"art_length":97.2, "art_width": 56.5, "art_height": 52.7}
-
-        Example 2:
-        Existing data, pick largest value
-        out_dimensions = {"art_length":20, "art_width": 20, "art_height": 52.7}
-        split_dimensions = [97.2, 56.5, 52.7]
-        measurement_direction_flags = ""
-
-        ->
-
-        """
-
-        measurement_directions = self.__get_measurement_directions(measurement_direction_flag, len(split_dimensions))
-
-        for index, flag in enumerate(measurement_directions):
-            if flag not in out_dimensions.keys():
-                out_dimensions[flag] = float(split_dimensions[index])
-                continue
-            existing_value = out_dimensions[flag]
-            out_dimensions[flag] = max([float(split_dimensions[index]), existing_value])
+            for direction in directions:
+                existing_value = out_dimensions.get(direction, "")
+                if existing_value:
+                    out_dimensions[direction] = max([float(value), existing_value])
+                else:
+                    out_dimensions[direction] = float(value)
 
     def __extract_values(self, text: str, out_dimensions: dict) -> None:
 
@@ -130,29 +159,45 @@ class DimensionExtractor:
         cm_data_split = self.__extract_cm_data(text)
 
         if self.__all_measurement_directions(cm_data_split):
-            self.__update_out_dimensions_by_order(cm_data_split, out_dimensions, "")
+            self.__map_cm_data_to_direction_flags([], cm_data_split, out_dimensions)
+            return
 
         # Check for mention of any possible measurement directions
-        measurement_direction_flag = self.__get_direction_id_flag(text)
+        measurement_direction_flags = self.__get_direction_id_flags(text)
 
-        # Speacial case for diameter
-        if self.__has_diameter_flag(measurement_direction_flag):
-            # for diameter translated to length and width required two dimensions
-            cm_data_split = cm_data_split * 2
+        # Ranged values
 
+        # update the out_dimensions
+        self.__map_cm_data_to_direction_flags(measurement_direction_flags, cm_data_split, out_dimensions)
 
-        self.__update_out_dimensions_by_order(cm_data_split, out_dimensions,
-                                              measurement_direction_flag)
+    def __remove_text_only_brackets(self, raw_dimensions: str) -> str:
+        """
+        Example:
+            L. 13 1/2 × Diam. (disk) 1 3/4 in. (34.3 × 4.4 cm)
+
+            ->
+            L. 13 1/2 × Diam.  1 3/4 in. (34.3 × 4.4 cm)
+        """
+
+        text_in_brackets = re.findall(self.ONLY_TEXT_BRACKETS, raw_dimensions)
+        if text_in_brackets:
+            return re.sub("|".join(text_in_brackets), '', raw_dimensions).replace("()", "")
+
+        return raw_dimensions
 
     def extractation(self, raw_dimensions: str) -> dict:
+
+        raw_dimensions = raw_dimensions.lower()
 
         out_dimensions = {}
 
         if not isinstance(raw_dimensions, str):
-            print(raw_dimensions)
             return out_dimensions
 
-        raw_dimensions_split_sets = re.split("|".join(self.DIMENSION_SPLITS), raw_dimensions)
+        # Remove any text only brackets.
+        raw_dimensions_without_text_brackets = self.__remove_text_only_brackets(raw_dimensions)
+
+        raw_dimensions_split_sets = re.split("|".join(self.DIMENSION_SPLITS), raw_dimensions_without_text_brackets)
 
         if self.OVERALL_FLAG in raw_dimensions:
             # Only first set of interest.
@@ -162,7 +207,3 @@ class DimensionExtractor:
             self.__extract_values(raw_dimension, out_dimensions)
 
         return out_dimensions
-
-
-
-
--- src/tests/data_pipeline/data_transform/test_dimension_extractor.py
+++ src/tests/data_pipeline/data_transform/test_dimension_extractor.py
@@ -31,6 +31,11 @@ class TestExecute(unittest.TestCase):
         output_1 = self.extractor.extractation(input_1)
         self.assertEqual(expected_1, output_1)
 
+        input_2 = "H. 37 1/2 in. (95.3 cm.)"
+        expected_2 = {"art_height": 95.3}
+        output_2 = self.extractor.extractation(input_2)
+        self.assertEqual(expected_2, output_2)
+
         input_2 = 'Diam. 5 3/4 in. (14.6 cm)'
         expected_2 = {"art_length": 14.6, "art_width": 14.6}
         output_2 = self.extractor.extractation(input_2)
@@ -55,7 +60,87 @@ class TestExecute(unittest.TestCase):
         output = self.extractor.extractation(input_0)
         self.assertEqual(expected, output)
 
-    # H. 4 1/4 in. (10.8); Diam. 14 1/2 in. (36.8 cm)
-    # Overall: 7 3/4 in., 220 Grams (19.7 cm, 7.073 Troy Ounces)
+    def test_imperial_units_in_brackets(self):
+        input_0 = '6 x 4 x 1 in. (15.2 x 10.2 x 2.5 cm)'
+        expected = {"art_length": 15.2, "art_width": 10.2, "art_height": 2.5}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_remove_text_in_brackets(self):
+        input_0 = 'L. 13 1/2 × Diam. (disk) 1 3/4 in. (34.3 × 4.4 cm)'
+        expected = {"art_length": 34.3, "art_width": 4.4}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_multi_cm_brackets(self):
+        input_0 = '11 in. (27.9 cm) x 11 1/2 in. (29.2 cm) x 8 1/4 in. (21 cm)'
+        expected = {"art_length": 27.9, "art_width": 29.2, "art_height": 21}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_only_cm_and_text_brackets(self):
+        input_0 = '6.2 x 10.3 cm (overall)'
+        expected = {"art_length": 6.2, "art_width": 10.3}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+        input_1 = "14 x 13.9 cm, wt. 87.17 g."
+        expected_1 = {"art_length": 14, "art_width": 13.9}
+        output_1 = self.extractor.extractation(input_1)
+        self.assertEqual(expected_1, output_1)
+
+    def test_imperial_in_brackets(self):
+        input_0 = 'L. 18 cm (7 1/16 in.)'
+        expected = {"art_length": 18}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_multi_cm_brackets_two_direction_flags(self):
+        input_0 = 'H: 9 7/8 (25.1 cm) x Diam. 8 1/4 in.(21 cm)'
+        expected = {'art_height': 25.1, 'art_length': 21.0, 'art_width': 21.0}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+        input_1 = "L. 19 x W. 1 1/2 inches (48.3 x 3.8 cm)"
+        expected_1 = {"art_length": 48.3, "art_width": 3.8}
+        output_1 = self.extractor.extractation(input_1)
+        self.assertEqual(expected_1, output_1)
+
+    def test_mult_values_two_directions(self):
+        input_0 = "D 10 cm x H 5.5 cm"
+        expected_0 = {"art_length": 10, "art_width": 10, 'art_height': 5.5}
+        output_0 = self.extractor.extractation(input_0)
+        self.assertEqual(expected_0, output_0)
+
+    def test_comma_separator(self):
+        input_0 = "10 5/8 × 16 3/8 × 8 7/8 in. (27 cm, 22.5 cm)"
+        expected = {'art_length': 27, 'art_width': 22.5}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test_weight_data(self):
+        input_0 = "Overall: 16 7/8 x 9 1/8 x 6 3/8 in., 9.068lb. (42.9 x 23.2 x 16.2 cm, 4113g)"
+        expected = {"art_length": 42.9, "art_width": 23.2, "art_height": 16.2}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+        input_1 = "6 × 6 5/8 × 2 3/4 in., 1.3 lb. (15.2 × 16.8 × 7 cm, 0.6 kg)"
+        expected_1 = {"art_length": 15.2, "art_width": 16.8, "art_height": 7}
+        output_1 = self.extractor.extractation(input_1)
+        self.assertEqual(expected_1, output_1)
+
+    def text_diam_plus_default_thickness(self):
+        input_0 = "Diam. 2 5/8 × 1/4 in., 0.3 lb. (6.7 × 0.6 cm, 0.1 kg)"
+        expected = {'art_length': 27, 'art_width': 22.5}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+    def test(self):
+        input_0 = """Approx. 10.2 x 6.3 cm (4 x 2 1/2 in.)"""
+        expected = {'art_length': 27, 'art_width': 22.5}
+        output = self.extractor.extractation(input_0)
+        self.assertEqual(expected, output)
+
+
 if __name__ == '__main__':
     unittest.main()
