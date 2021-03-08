3356b61e45cb85648569845ce2d28b47e699d708
jwd <jwd.devlin@gmail.com>
2021-03-08 23:03:02 +0100

    App logic

--- src/main/art_processor/art_processor.py
+++ src/main/art_processor/art_processor.py
@@ -2,27 +2,33 @@ from src.main.data_pipeline.storage.art_storage import ArtStorage
 import logging
 
 
-class ArtProccessor:
+class ArtProcessor:
     DEFAULT_MEASUREMENT_DIRECTIONS = ["art_length", "art_width", "art_height"]
 
     def __init__(self, art_storage: ArtStorage):
         self.art_storage = art_storage
 
-    def __check_no_dimensions(self, art_dimensions):
-        return any(art_dimensions.loc[0, ["art_length", "art_width", "art_height"]].tolist())
+    def __check_dimensions(self, art_dimensions: dict, dimensions_user: dict) -> bool:
+        for key, value in art_dimensions.items():
+            if value > dimensions_user[key]:
+                return False
+        return True
 
-    def __check_dimensions(self, dimensions, art_dimensions):
-        pass
-
-    def does_it_fit(self, art_object_id: int, dimensions: dict) -> bool:
-
-        art_dimensions = self.art_storage.get_dimensions(art_object_id)
+    def __does_it_fit_python(self, art_object_id: int, dimensions_user: dict) -> bool:
+        art_dimensions = self.art_storage.read_dimensions_by_object_id(art_object_id)
 
         # No art
-        if art_dimensions.emtpy:
+        if art_dimensions.empty:
             logging.info("No art for this id: %s", art_object_id)
             return False
 
-        # No Dimensions
-        if self.___check_no_dimensions(art_dimensions):
-            return False
+        return self.__check_dimensions(art_dimensions.loc[0, self.DEFAULT_MEASUREMENT_DIRECTIONS].to_dict(),
+                                       dimensions_user)
+
+    def does_it_fit(self, art_object_id: int, dimensions_user: dict, python: bool = True) -> bool:
+
+        if python:
+            return self.__does_it_fit_python(art_object_id, dimensions_user)
+
+        dimensions_user_ordered = [dimensions_user[key] for key in self.DEFAULT_MEASUREMENT_DIRECTIONS]
+        return self.art_storage.check_fit_by_object_id_and_dimensions(art_object_id, dimensions_user_ordered)
